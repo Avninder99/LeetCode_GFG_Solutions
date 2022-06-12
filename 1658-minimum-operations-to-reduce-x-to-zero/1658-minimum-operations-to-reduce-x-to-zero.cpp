ffe2b68df48d76ca3e1601231b4dcb18f9819d57
Avninder Preet Singh <avninderpreet99@gmail.com>
2022-06-12 18:53:13 +0530

    Time: 478 ms (37.52%), Space: 179 MB (6.03%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
@@ -14,13 +14,6 @@ public:
             }
         }
         for(int i=0;i<size;i++){
-            // for(int j=0;j<size-i-1;j++){
-            //     if(l[i] + r[j] == x){
-            //         if(ans2 == -1 || ans2 > i+j+2)
-            //             ans2 = i + j + 2;
-            //     }
-            //     else if(l[i] + r[j] > x) break;
-            // }
             auto itr = r.find(x-l[i]);
             if(itr != r.end() && itr->second < (size - i - 1)){
                 if(ans2 == -1 || ans2 > i + itr->second + 2)
@@ -30,30 +23,5 @@ public:
         if(ans1 >= 0 && ans2 >= 0) return min(ans1, ans2);
         else if(ans1 >= 0) return ans1;
         else return ans2;
-        // for(int i=0;i<size;i++){
-        //     for(int j=0;j<size-i-1;j++){
-        //         if(l[j] + r[i] == x){
-        //             if(ans3 == -1 || ans3 > i+j+2)
-        //                 ans3 = i + j + 2;
-        //         }
-        //         else if(l[i] + r[j] > x) break;
-        //     }
-        // }
-        // cout << ans1 << " $ " << ans2 << " & " << ans3 << endl;
-        // if(ans1 >= 0 && ans2 >= 0 && ans3 >= 0) return min(ans1, min(ans2,ans3));
-        // else if(ans1 >= 0){
-        //     if(ans2 >= 0){
-        //         return min(ans1, ans2);
-        //     }else if(ans3 >= 0){
-        //         return min(ans1, ans3);
-        //     }else return ans1;
-        // }else if(ans2 >= 0){
-        //     if(ans3 >= 0){
-        //         return min(ans2, ans3);
-        //     }else{
-        //         return ans2;
-        //     }
-        // }
-        // else return ans3;
     }
 };
