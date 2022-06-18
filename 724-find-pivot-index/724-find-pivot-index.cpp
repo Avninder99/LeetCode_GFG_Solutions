f8807203a777a8682f7b28ebc744118d8cc4485a
Avninder Preet Singh <avninderpreet99@gmail.com>
2022-06-18 12:41:03 +0530

    Time: 30 ms (71.85%), Space: 31.2 MB (19.03%) - LeetHub

---
+++ 724-find-pivot-index/724-find-pivot-index.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int size = nums.size(), rtotal=0, ltotal = 0;
+        for(int i=0;i<size;i++){
+            rtotal += nums[i];
+        }
+        for(int i=0;i<size;i++){
+            if(ltotal == rtotal - nums[i] - ltotal){
+                return i;
+            }
+            ltotal += nums[i];
+        }
+        return -1;
+    }
+};
