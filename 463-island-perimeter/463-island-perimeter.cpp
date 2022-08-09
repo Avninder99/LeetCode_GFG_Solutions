06478b94eec6a7c42d02a5e99d4c1b7d6fb9e14a
Avninder Preet Singh <avninderpreet99@gmail.com>
2022-08-09 10:11:48 +0530

    Time: 187 ms (46.28%), Space: 96.2 MB (87.15%) - LeetHub

---
+++ 463-island-perimeter/463-island-perimeter.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int islandPerimeter(vector<vector<int>>& g) {
+        int pVal = 0, csize=g.size(), rsize=g[0].size(),temp;
+        for(int i=0;i<csize;i++){
+            for(int j=0;j<rsize;j++){
+                if(g[i][j]==1){
+                    // temp=0;
+                    if(i==0 || g[i-1][j]==0){
+                        pVal++;
+                    }
+                    if(j==rsize-1 || g[i][j+1]==0){
+                        pVal++;
+                    }
+                    if(i==csize-1 || g[i+1][j]==0){
+                        pVal++;
+                    }
+                    if(j==0 || g[i][j-1]==0){
+                        pVal++;
+                    }    
+                }
+            }
+        }
+        return pVal;
+    }
+};
