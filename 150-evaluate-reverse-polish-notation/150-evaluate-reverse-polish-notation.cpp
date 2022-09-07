36c13e4a62ba1b061369f4890a699050e949868c
Avninder Preet Singh <avninderpreet99@gmail.com>
2022-09-07 21:27:43 +0530

    Time: 16 ms (58.45%), Space: 11.8 MB (89.38%) - LeetHub

--- 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.cpp
+++ 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.cpp
@@ -4,30 +4,20 @@ public:
         int size = t.size(), res, temp, n1, n2;
         stack<int>st;
         for(int i=0;i<size;i++){
-            if(t[i] == "+"){
+            if(t[i] == "+" || t[i] == "*"){
                 n1 = st.top();
                 st.pop();
                 n2 = st.top();
                 st.pop();
-                st.push(n1 + n2);
-            }else if(t[i] == "-"){
+                if(t[i] == "+") st.push(n1 + n2);
+                else st.push(n1 * n2);
+            }else if(t[i] == "-" || t[i] == "/"){
                 n1 = st.top();
                 st.pop();
                 n2 = st.top();
                 st.pop();
-                st.push(n2 - n1);
-            }else if(t[i] == "*"){
-                n1 = st.top();
-                st.pop();
-                n2 = st.top();
-                st.pop();
-                st.push(n1 * n2);
-            }else if(t[i] == "/"){
-                n1 = st.top();
-                st.pop();
-                n2 = st.top();
-                st.pop();
-                st.push(n2 / n1);
+                if(t[i] == "-") st.push(n2 - n1);
+                else st.push(n2 / n1);
             }else{
                 st.push(stoi(t[i]));
             }
