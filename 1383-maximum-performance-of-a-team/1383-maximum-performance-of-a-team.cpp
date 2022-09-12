70a6ced04f0ca8c6a35d011994d5843c1c812800
Avninder Preet Singh <avninderpreet99@gmail.com>
2022-09-12 12:17:49 +0530

    Time: 367 ms (5.16%), Space: 46.9 MB (12.03%) - LeetHub

--- 1383-maximum-performance-of-a-team/1383-maximum-performance-of-a-team.cpp
+++ 1383-maximum-performance-of-a-team/1383-maximum-performance-of-a-team.cpp
@@ -3,58 +3,34 @@
 // };
 class Solution {
 public:
-    int maxPerformance(int n, vector<int>& speed, vector<int>& efficiency, int k) {
-        vector<pair<int, int>> ess(n);
-        for (int i = 0; i < n; ++i)
-            ess[i] = {efficiency[i], speed[i]};
-        sort(rbegin(ess), rend(ess));
-        long sumS = 0, res = 0;
-        priority_queue <int, vector<int>, greater<int>> pq; //min heap
-        for(auto& [e, s]: ess){
-            pq.emplace(s);
-            sumS += s;
-            if (pq.size() > k) {
-                sumS -= pq.top();
-                pq.pop();
-            }
-            res = max(res, sumS * e);
-        }
-        return res % (int)(1e9+7);
-    }
-    // code v1
-//     static bool comp(vector<int>&a, vector<int>&b){
-//         if(a[1] != b[1]) return a[1] > b[1];
-//         return a[0] > b[0];
-//     }
     
-//     int maxPerformance(int n, vector<int>& s, vector<int>& e, int k) {
-//         int size = s.size();
-//         vector<vector<int>>v1;
-//         for(int i=0;i<size;i++){
-//             v1.push_back({s[i], e[i]});
-//         }
-//         sort(v1.begin(), v1.end(), comp);
-//         priority_queue<int, vector<int>, decltype(comp2)> pq(comp2);
-//         int cs=0, me=1;
-//         int res=0;
-//         for(int i=0;i<size;i++){
-//             if(pq.size() < k){
-//                 pq.push(v1[i][0]);
-//                 cs += v1[i][0];
-//                 me = v1[i][1];
-//                 res = max( res, cs*me );
-//             }
-//             else{
-//                 int temp = pq.top();
-//                 pq.pop();
-//                 me = v1[i][1];
-//                 pq.push(v1[i][0]);
-//                 cs = cs - temp + v1[i][0];
-//                 res = max(res, cs*me);
-//             }
-//         }
-//         return res;
-        
+    int maxPerformance(int n, vector<int>& s, vector<int>& e, int k) {
+        vector<vector<int>>v1;
+        for(int i=0;i<n;i++){
+            v1.push_back({e[i],s[i]});
+        }
+        sort(rbegin(v1), rend(v1));
+        priority_queue<int, vector<int>, greater<int>> pq;
+        long long int cs=0, me=1, res=0;
+        for(int i=0;i<n;i++){
+            pq.emplace(v1[i][1]);
+            cs += v1[i][1];
+            if(pq.size() > k){
+                cs -= pq.top();
+                pq.pop();
+                
+            }
+            res = max( res, cs*v1[i][0] );
+        }
+        return res%1000000007;
+    }
+    
+    // code v1
+    // static bool comp(vector<int>&a, vector<int>&b){
+    //     if(a[1] != b[1]) return a[1] > b[1];
+    //     return a[0] > b[0];
+    // }
+    // int maxPerformance(int n, vector<int>& s, vector<int>& e, int k) {
         // for(int i=size-2;i>=0;i--){
         //     if(pq.size() < k){
         //         int tme = min(me, v1[i][1]);
@@ -84,5 +60,6 @@ public:
         // // }
         // // for(int i=0;i<;i++)
         // return cs*me;
+    // }
     // }
 };
