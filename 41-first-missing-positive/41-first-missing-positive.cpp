a3075ada08a38a1d0740161e2b9e56bebae163be
Avninder Preet Singh <avninderpreet99@gmail.com>
2022-09-13 12:19:03 +0530

    Time: 65 ms (86.78%), Space: 31.2 MB (90.08%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.cpp
+++ 41-first-missing-positive/41-first-missing-positive.cpp
@@ -2,34 +2,25 @@ class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
         int size = nums.size(), curr;
-        // for(int i=0;i<size;i++){
-        int t=0, i=0;
+        int i=0, temp;
         while(i < size){
             while(i < size && (nums[i] < 1 || nums[i] == i+1)) i++;
             if(i >= size) break;
             // nums[i] == i+1 element exist (custom condition)
             curr = nums[i];
             nums[i] = -1;
-            // t++;
             if(curr <= size){
                 if(nums[curr-1] != curr){
-                    int temp = nums[curr-1];
-                    nums[curr-1] = curr;
-                    curr = temp;
-                    // t++;
-                    while(curr > 0 && curr <= size && nums[curr-1] != curr){
+                    do{
                         temp = nums[curr-1];
                         nums[curr-1] = curr;
-                        curr = temp;
-                        // t++;
-                    }    
+                        curr = temp;    
+                    }while(curr > 0 && curr <= size && nums[curr-1] != curr);  
                 } 
             }
-            // i++;
         }
         
         for(int j=0;j<size;j++){
-            cout << nums[j] << " ";
             if(nums[j] != j+1) return j+1;   
         }
         return size+1;
