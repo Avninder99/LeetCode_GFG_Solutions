f9efe8349f3b14b6110cccf70606fb080b1ce7e4
Avninder Preet Singh <avninderpreet99@gmail.com>
2022-09-14 19:39:57 +0530

    Time: 1241 ms (12.49%), Space: 304.5 MB (22.28%) - LeetHub

--- 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.cpp
+++ 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.cpp
@@ -14,16 +14,22 @@ public:
     
     void traverser(TreeNode* curr, int &counter, unordered_set<int>&s1){
         if(!curr) return;
-        if(s1.count(curr->val)) s1.erase(curr->val);
-        else s1.insert(curr->val);
+        bool e;
+        if(s1.count(curr->val)){
+            s1.erase(curr->val);
+            e = true;
+        }else{
+            s1.insert(curr->val);
+            e = false;
+        }
         if(!curr->left && !curr->right){
             if(s1.size() <= 1) counter++;
         }else{
             traverser(curr->left, counter, s1);
             traverser(curr->right, counter, s1);    
         }
-        if(s1.count(curr->val)) s1.erase(curr->val);
-        else s1.insert(curr->val);
+        if(e) s1.insert(curr->val); 
+        else s1.erase(curr->val);
         return;
     }
     int pseudoPalindromicPaths (TreeNode* root) {
