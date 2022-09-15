db5524c986544946877a3664861d28db566032a9
Avninder Preet Singh <avninderpreet99@gmail.com>
2022-09-15 14:58:37 +0530

    Time: 4 ms (22.56%), Space: 6.1 MB (31.79%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,7 +1,36 @@
 class Solution {
 public:
+    // Memorization
+//     int func1(int num, int &n, vector<int>&dp){
+//         if(num == n){
+//             return 1;
+//         }else if(num > n) return 0;
+        
+//         int one = 0, two = 0;
+        
+//         if(dp[num] != -1){
+//             one = dp[num];
+//         }else{
+//             one = func1(num+1, n, dp);
+//             dp[num] = one;
+//         }
+        
+//         if(num+1 < n){
+//             if(dp[num+1] != -1){
+//                 two = dp[num+1];
+//             }else{
+//                 two = func1(num+2, n, dp);
+//                 dp[num+1] = two;
+//             }
+//         }
+        
+//         return one + two;
+//     }
+    
     int climbStairs(int n) {
         vector<int>dp(n, 0);
+        // int ans = func1(0, n, dp);
+        // return ans;
         // Tabulation
         if(n==1) return 1;
         dp[n-1] = 1, dp[n-2] = 2;
