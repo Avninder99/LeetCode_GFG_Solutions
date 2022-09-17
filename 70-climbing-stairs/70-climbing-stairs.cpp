5693fb7e29457b6970e836ed6ca8d59f846742f0
Avninder Preet Singh <avninderpreet99@gmail.com>
2022-09-17 15:19:31 +0530

    Time: 2 ms (42.81%), Space: 5.9 MB (57.15%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,42 +1,14 @@
 class Solution {
 public:
-    // Memorization
-//     int func1(int num, int &n, vector<int>&dp){
-//         if(num == n){
-//             return 1;
-//         }else if(num > n) return 0;
-        
-//         int one = 0, two = 0;
-        
-//         if(dp[num] != -1){
-//             one = dp[num];
-//         }else{
-//             one = func1(num+1, n, dp);
-//             dp[num] = one;
-//         }
-        
-//         if(num+1 < n){
-//             if(dp[num+1] != -1){
-//                 two = dp[num+1];
-//             }else{
-//                 two = func1(num+2, n, dp);
-//                 dp[num+1] = two;
-//             }
-//         }
-        
-//         return one + two;
-//     }
-    
     int climbStairs(int n) {
-        vector<int>dp(n, 0);
-        // int ans = func1(0, n, dp);
-        // return ans;
-        // Tabulation
-        if(n==1) return 1;
-        dp[n-1] = 1, dp[n-2] = 2;
+        // Optimal Approach
+        if(n <= 2) return n;
+        int next=2, next2=1, curr;
         for(int i=n-3;i>=0;i--){
-            dp[i] = dp[i+2] + dp[i+1]; 
+            curr = next + next2;
+            next2 = next;
+            next = curr;
         }
-        return dp[0];
+        return curr;
     }
 };
