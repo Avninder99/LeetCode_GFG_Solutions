e81e41b9f6a9cd19381f4b2a9cd4be5a32bae3ed
Avninder Preet Singh <avninderpreet99@gmail.com>
2022-11-13 00:46:51 +0530

    Time: 27 ms (60.06%), Space: 21.1 MB (66.34%) - LeetHub

---
+++ 1123-lowest-common-ancestor-of-deepest-leaves/1123-lowest-common-ancestor-of-deepest-leaves.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* lcaDeepestLeaves(TreeNode* root) {
+        holder = make_pair(root, -1);
+        func(root, 0);
+        return holder.first;
+    }
+private:
+    pair<TreeNode* , int> holder;
+    int func(TreeNode* curr, int d){
+        if(!curr){
+            return d-1;
+        }
+        
+        int l = func(curr->left, d+1);
+        int r = func(curr->right, d+1);
+        
+        if(l == r && l >= holder.second){
+            holder = make_pair(curr, l);
+        }
+        
+        return max(l, r);
+    }
+};
