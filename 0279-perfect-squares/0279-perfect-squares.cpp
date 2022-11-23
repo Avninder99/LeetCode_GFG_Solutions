71a0b376d6a7cae9e6a0b64ea431527674027301
Avninder Preet Singh <avninderpreet99@gmail.com>
2022-11-23 10:08:55 +0530

    Time: 116 ms (87.03%), Space: 9.3 MB (41.64%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -22,14 +22,28 @@ public:
     }
     int numSquares(int n) {
         vector<int>sq;
-        vector<int> dp(n + 1, -1);
+        vector<int> dp(n + 1, 0);
+        // dp[0] = 0;
         int i = 1;
         while(i*i <= n){
             sq.push_back(i*i);
             i++;
         }
         
-        return solve(n, sq, dp);
+        for (int t = 1; t<=n; t++) {
+            int ans = INT_MAX;
+            for (int s : sq) {
+                if (s > t)
+                    break;
+                
+                ans = min(ans, 1 + dp[t - s]);
+            }
+            
+            dp[t] = ans;
+        }
+        
+        
+        return dp[n];
         
     }
 };
