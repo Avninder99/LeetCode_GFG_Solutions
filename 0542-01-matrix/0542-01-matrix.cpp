1e22a1e7af56c92e325172e3631cbb031ae4a173
Avninder Preet Singh <avninderpreet99@gmail.com>
2022-11-26 04:00:21 +0530

    Time: 148 ms (68.48%), Space: 27.6 MB (80.76%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,41 +1,41 @@
 class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>&g) {
+        // Tabulation Approach
         int rs = g[0].size(), cs = g.size(), step = 1;
-	    vector<vector<int>>res(cs, vector<int>(rs, -1));
-	    queue<vector<int>>q1;
-	    for(int i=0;i<cs;i++){
-	        for(int j=0;j<rs;j++){
-	            if(!g[i][j]){
-	                q1.push({i,j});
-	                res[i][j] = 0;
-	            }
-	        }
-	    }
-	    
-	    vector<vector<int>>dir;
-	    dir.push_back({0, -1});
-	    dir.push_back({1, 0});
-	    dir.push_back({0, 1});
-	    dir.push_back({-1, 0});
-	    
-	    q1.push({-1,-1});
-	    while(q1.size() > 1){
-	        if(q1.front()[0] == -1){
-	            q1.pop();
-	            q1.push({-1,-1});
-	            step++;
-	        }else{
-	            for(int i=0;i<4;i++){
-    	            int tc = q1.front()[0] + dir[i][1], tr = q1.front()[1] + dir[i][0];
-    	            if(tc >= 0 && tr >= 0 && tc < cs && tr < rs && res[tc][tr] == -1){
-    	                q1.push({tc, tr});
-    	                res[tc][tr] = step;
-    	            }
-	            }
-	            q1.pop();
-	        }
-	    }
-	    return res;
+	    vector<vector<int>>res(cs, vector<int>(rs, 100000));
+        
+        if(g[cs-1][rs-1] == 0)
+            res[cs-1][rs-1] = 0;
+        
+        for(int i=cs-2;i>=0;i--){
+            res[i][rs-1] = (g[i][rs-1] == 0) ? 0 : res[i+1][rs-1]+1;
+        }
+        
+        for(int i=rs-2;i>=0;i--){
+            res[cs-1][i] = (g[cs-1][i] == 0) ? 0 : res[cs-1][i+1]+1;
+        }
+        
+        for(int i=cs-2;i>=0;i--){
+            for(int j=rs-2;j>=0;j--){
+                res[i][j] = (g[i][j] == 0) ? 0 : (min(res[i+1][j], res[i][j+1]) + 1);
+            }
+        }
+        
+        for(int i=1;i<rs;i++){
+            res[0][i] = (g[0][i] == 0) ? 0 : min(res[0][i-1]+1, res[0][i]);
+        }
+        
+        for(int i=1;i<cs;i++){
+            res[i][0] = (g[i][0] == 0) ? 0 : min(res[i-1][0]+1, res[i][0]);
+        }
+        
+        for(int i=1;i<cs;i++){
+            for(int j=1;j<rs;j++){
+                res[i][j] = (g[i][j] == 0) ? 0 : min((min(res[i-1][j], res[i][j-1]) + 1), res[i][j]);
+            }
+        }
+        
+        return res;
     }
 };
