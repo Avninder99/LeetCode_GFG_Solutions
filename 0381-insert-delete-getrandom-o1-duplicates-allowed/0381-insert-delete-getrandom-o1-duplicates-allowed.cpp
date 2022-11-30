11efa83b63395f6ffd5945e977e4972bc815263f
Avninder Preet Singh <avninderpreet99@gmail.com>
2022-11-30 21:15:49 +0530

    Time: 754 ms (6.59%), Space: 125.4 MB (5.08%) - LeetHub

---
+++ 0381-insert-delete-getrandom-o1-duplicates-allowed/0381-insert-delete-getrandom-o1-duplicates-allowed.cpp
@@ -0,0 +1,80 @@
+class RandomizedCollection {
+public:
+    vector<int>rec;
+    unordered_map<int, unordered_set<int>>m1;
+    int seed = 1234567;
+    RandomizedCollection() {
+        
+    }
+    
+    bool insert(int val) {
+        int t = rec.size();
+        rec.push_back(val);
+        bool retVal = false;
+        
+        if(m1.find(val) == m1.end() || m1[val].size() == 0){
+            retVal = true;
+        }
+        
+        m1[val].insert(t);
+        
+        return retVal;
+    }
+    
+    bool remove(int val) {
+        if(m1.find(val) == m1.end() || m1[val].size() == 0){
+            return false;
+        }
+        
+        if(rec[rec.size() - 1] == val){
+            m1[val].erase(rec.size() - 1);
+            rec.pop_back();
+            return true;
+        }
+        
+        int targetI = *(m1[val].begin());
+        
+        int lVal = rec[rec.size() - 1];
+        m1[lVal].erase(rec.size() - 1);
+        
+        m1[val].erase(targetI);
+        
+        m1[lVal].insert(targetI);
+        rec[targetI] = lVal;
+        
+        rec.pop_back();
+        
+        return true;
+    }
+    
+    int getRandom() {
+        srand(seed);
+        seed += 10;
+        int r = rand()%rec.size();
+        int lVal = rec[rec.size() - 1], rVal = rec[r];
+        
+        if(lVal == rVal){
+            return rVal;
+        }
+        
+        m1[lVal].erase(rec.size() - 1);
+        m1[rVal].erase(r);
+        
+        m1[lVal].insert(r);
+        m1[rVal].insert(rec.size() - 1);
+        
+        rec[r] = lVal;
+        rec[rec.size() - 1] = rVal;
+        
+        return rVal;
+    }
+};
+
+
+/**
+ * Your RandomizedCollection object will be instantiated and called as such:
+ * RandomizedCollection* obj = new RandomizedCollection();
+ * bool param_1 = obj->insert(val);
+ * bool param_2 = obj->remove(val);
+ * int param_3 = obj->getRandom();
+ */
