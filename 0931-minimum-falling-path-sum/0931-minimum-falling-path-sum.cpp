3579977452f87da166c32759e10629ce8d757b4b
Avninder Preet Singh <avninderpreet99@gmail.com>
2022-12-13 11:53:15 +0530

    Time: 13 ms (95.69%), Space: 9.8 MB (95.85%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -1,47 +1,16 @@
 class Solution {
 public:
-    int minFallingPathSum(vector<vector<int>>& m) 
-    {
-        int n = m.size();
-        
-        for(int row = 1; row < n; row++)
-        {
-            for(int col = 0; col < n; col++)
-                
-            {
-                // int rowUp, leftUp, rightUp;
-                
-                int rowUp = m[row - 1][col];
-                int leftUp = col > 0 ? m[row-1][col-1] : INT_MAX;
-                int rightUp = col < n-1 ? m[row-1][col+1] : INT_MAX;
-                
-                // cout << rowUp << leftUp << rightUp << endl;
-                
-                m[row][col] += min(rowUp, min(leftUp, rightUp));
+    int minFallingPathSum(vector<vector<int>>& m) {
+        int s = m.size();
+        for(int i=s-2; i>=0; i--){
+            for(int j=0; j<s; j++){
+                m[i][j] += min(m[i+1][j], min( (j == 0) ? 10001 : m[i+1][j-1], (j == s-1) ? 10001 : m[i+1][j+1] ));
             }
         }
-        int ans = INT_MAX;
-        int end = n-1;
-        
-        for(int col = 0; col < n; col++)
-        {
-            ans = min(ans, m[end][col]);
+        int mini = INT_MAX;
+        for(int i=0;i<s;i++){
+            mini = min(mini, m[0][i]);
         }
-        
-        return ans;
+        return mini;
     }
-    
-    // int minFallingPathSum(vector<vector<int>>& m) {
-    //     int s = m.size();
-    //     for(int i=s-2; i>=0; i--){
-    //         for(int j=0; j<s; j++){
-    //             m[i][j] += min(m[i+1][j], min( (j < 1) ? 200 : m[i+1][j-1], (j+1 >= s) ? 200 : m[i+1][j+1] ));
-    //         }
-    //     }
-    //     int mini = INT_MAX;
-    //     for(int i=0;i<s;i++){
-    //         mini = min(mini, m[0][i]);
-    //     }
-    //     return mini;
-    // }
 };
