34712eae33e7d397f55d12558d0111ecf73a656d
Avninder Preet Singh <avninderpreet99@gmail.com>
2022-12-18 23:46:42 +0530

    Time: 488 ms (17.49%), Space: 102.9 MB (53.89%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
     vector<int> dailyTemperatures(vector<int>& t) {
+        // similar to next greater integer element in an array question so use monotonic stack
+        vector<int>res(t.size(), 0);
         stack<int>st;
         for(int i=0;i<t.size();i++){
-            while(!st.empty() && t[st.top()] < t[i]){
-                t[st.top()] = i - st.top();
+            while(!st.empty() && t[i] > t[st.top()]){
+                res[st.top()] = i - st.top();
                 st.pop();
             }
             st.push(i);
         }
-        while(!st.empty()){
-            t[st.top()] = 0;
-            st.pop();
-        }
-        return t;
+        return res;
     }
 };
