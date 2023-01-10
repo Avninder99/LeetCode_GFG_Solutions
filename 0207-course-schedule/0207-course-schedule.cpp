d6b9e4ad021dd8ec119db107f7b41e30d7731f92
Avninder Preet Singh <avninderpreet99@gmail.com>
2023-01-10 15:05:20 +0530

    Time: 30 ms (63.40%), Space: 14.6 MB (26.06%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,38 +1,34 @@
 class Solution {
 public:
-    bool dfs(int curr, vector<int>&vis, vector<vector<int>>&adj, vector<int>&rec){
+    bool dfs(int curr, vector<int>&vis, vector<vector<int>>&adj){
         vis[curr] = 1;
         for(auto itr: adj[curr]){
+            if(vis[itr] == 2) continue;
             if(vis[itr] == 0){
-                if(rec[itr] == 1) continue;
-                if(!dfs(itr, vis, adj, rec)){
+                if(!dfs(itr, vis, adj)){
                     return false;
                 }
             }
             else return false;
         }
-        rec[curr] = 1;
-        vis[curr] = 0;
+        vis[curr] = 2;
         return true;
     }
     bool canFinish(int nc, vector<vector<int>>& p) {
-        vector<int>vis(nc, 0), rec(nc, 0);
-        // rec -> 0 (not traversed), 1 (safe/ no need to traverse), 2 (loop)
-        // vis stores visited record only during a single traversaL and gets reset every time
-        // rec store if the node was traversed in previous iterations is does not contain a loop
-        
-        // rec is used to reduce time complexity from O(N^2) to O(N)
-        // vis is used to find loop in a single traversal
-        
+        vector<int>vis(nc, 0);
+        // 0 -> not visited
+        // 1 -> visited in this iteration (all children nodes haven't been traversed yet)
+        // 2 -> safe node (visited in previous iteration, No child contains self loop)
         vector<vector<int>>adj(nc);
         for(auto itr: p){
             adj[itr[1]].push_back(itr[0]);
         }
         bool res = true;
         for(int i=0;i<nc;i++){
-            if(vis[i] == 0 && rec[i] == 0){
-                res = dfs(i, vis, adj, rec);
-                if(!res) return false;
+            if(vis[i] == 0){
+                if(!dfs(i, vis, adj)){
+                    return false;
+                }
             }
         }
         return true;
