497df1bf0a07de90d1b50d98e395e04d6c2ab1bb
Avninder Preet Singh <avninderpreet99@gmail.com>
2023-02-01 03:33:39 +0530

    Time: 188 ms (74.44%), Space: 20 MB (51.13%) - LeetHub

--- 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.cpp
+++ 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     int bestTeamScore(vector<int>& scores, vector<int>& ages) {
-        vector<pair<int,int>> arr; // Create a array of pairs of age & scores
-        vector<int> ans ((int)scores.size(), 0); // An array to hold answers of LIS ending at given index-- More formally, Longest Non decreasing Subsequence
+        
+        // Not mine
+        
+        vector<pair<int,int>> arr;
+        vector<int> ans ((int)scores.size(), 0);
         
         for(int i=0;i<scores.size();i++) {
-            arr.push_back({ages[i], scores[i]});  // Push {age, score} pair in the array
+            arr.push_back({ages[i], scores[i]});
         }
-        sort(arr.begin(), arr.end()); // It is important that age is the first element AND score is the second element  in the pair. This is because in sorting vector of pairs, pair.first is given priority over pair.second
+        sort(arr.begin(), arr.end());
         
-        // Note: From here on we dont need age at all! We only access arr[i].second to access scores! 
-        int best = 0;   // Store the final answer
-        
-        // Logic of LIS
+        int best = 0;
         for(int i=0; i<arr.size();i++) {
             ans[i]=arr[i].second; 
             for(int j=0;j<i;j++) {
@@ -23,6 +23,6 @@ public:
             best = max(best, ans[i]);
         }
         
-        return best; // Time to celebrate!
+        return best;
     }
 };
