23df587fab2ac1adbf4591a47449a088f0934cc1
Avninder Preet Singh <avninderpreet99@gmail.com>
2023-02-08 12:52:17 +0530

    Time: 131 ms (90.16%), Space: 66.1 MB (96.00%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -1,13 +1,30 @@
 class Solution {
 public:
-    int totalFruit(vector<int> tree) {
-        int res = 0, cur = 0, count_b = 0, a = 0, b = 0;
-        for (int c :  tree) {
-            cur = c == a || c == b ? cur + 1 : count_b + 1;
-            count_b = c == b ? count_b + 1 : 1;
-            if (b != c) a = b, b = c;
-            res = max(res, cur);
+    int totalFruit(vector<int>& f) {
+        if(f.size() < 3) return f.size();
+        int e1 = 0, e2 = -1, n1 = f[0], n2 = -1, c1 = 1, c2 = 0, rec = 2;
+        for(int i=1;i<f.size();i++){
+            if(f[i] == n1){
+                c1++;
+                e1 = i;
+            }else if(f[i] == n2){
+                c2++;
+                e2 = i;
+            }else{
+                if(e1 < e2){
+                    c2 = e2 - e1;
+                    e1 = i;
+                    n1 = f[i];
+                    c1 = 1;
+                }else{
+                    c1 = e1 - e2;
+                    e2 = i;
+                    n2 = f[i];
+                    c2 = 1;
+                }
+            }
+            rec = max(rec, c1 + c2);
         }
-        return res;
+        return rec;
     }
 };
