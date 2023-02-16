ff13972f5c82fa04688b93de57878f29a6a097d6
Avninder Preet Singh <avninderpreet99@gmail.com>
2023-02-16 21:03:38 +0530

    Time: 78 ms (92.45%), Space: 16.2 MB (14.19%) - LeetHub

---
+++ 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    int findTheCity(int n, vector<vector<int>>& edges, int distT) {
+        pair<int, int>res = {-1, 1e9};
+        vector<vector<pair<int, int>>>adj(n);
+        for(auto itr: edges){
+            adj[itr[0]].push_back({itr[1], itr[2]});
+            adj[itr[1]].push_back({itr[0], itr[2]});
+        }
+        // {distance, curr_node}
+        
+        for(int src=0;src<n;src++){
+            vector<int>records(n, 1e9);
+            records[src] = 0;
+            priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>>pq;
+            
+            pq.push({0, src});
+            
+            while(!pq.empty()){
+                pair<int, int>holder = pq.top();
+                pq.pop();
+                
+                if(holder.first > distT){
+                    break;
+                }
+                
+                for(auto itr: adj[holder.second]){
+                    int toNode = itr.first, dist = itr.second;
+                    if(holder.first + dist < records[toNode]){
+                        records[toNode] = holder.first + dist;
+                        pq.push({records[toNode], toNode});
+                    }
+                }
+            }
+            
+            int count = 0;
+            for(auto itr: records){
+                if(itr > 0 && itr <= distT) count++;
+            }
+            if(count <= res.second){
+                res = {src, count};
+            }
+        }
+        return res.first;
+    }
+};
