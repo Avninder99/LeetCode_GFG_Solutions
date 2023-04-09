e2412aa527a46dd2927874f4885daac4d7f1e666
Avninder Preet Singh <avninderpreet99@gmail.com>
2023-04-09 23:43:01 +0530

    Added solution - LeetHub

--- Coin Change - GFG/coin-change.cpp
+++ Coin Change - GFG/coin-change.cpp
@@ -8,30 +8,60 @@ class Solution {
     long long int count(int coins[], int N, int sum) {
 
         // code here.
-        vector<vector<long long int>>dp(sum + 1, vector<long long int>(N + 1, -1));
-        return func(coins, N, sum, dp);
-    }
-    long long int func(int coins[], int index, int lsum, vector<vector<long long int>>&dp){
-        if(lsum == 0){
-            return 1;
+        
+        // DP Memoization
+        // vector<vector<long long int>>dp(sum + 1, vector<long long int>(N + 1, -1));
+        // return func(coins, N, sum, dp);
+        
+        // DP Tabulation
+        vector<vector<long long int>>t(sum + 1, vector<long long int>(N + 1, -1));
+        
+        for(int i=0;i<sum+1;i++){
+            t[i][0] = 0;
+        }
+        for(int i=0;i<N+1;i++){
+            t[0][i] = 1;
         }
         
-        if(index == 0){
-            return 0;
-        }
-        
-        if(dp[lsum][index] == -1){
-            long long int p = 0, np = 0;
-            
-            if(coins[index - 1] <= lsum){
-                p = func(coins, index, lsum - coins[index - 1], dp);
+        long long int p, np;
+        for(int i=1;i<=sum;i++){
+            for(int j=1;j<=N;j++){
+                p = 0, np = 0;
+                if(coins[j - 1] <= i){
+                    p = t[i - coins[j - 1]][j];
+                }
+                np = t[i][j - 1];
+                
+                t[i][j] = p + np;
             }
-            np = func(coins, index - 1, lsum, dp);
-            
-            dp[lsum][index] = p + np;
         }
-        return dp[lsum][index];
+        
+        return t[sum][N];
     }
+    
+    // DP Memoization function
+    
+    // long long int func(int coins[], int index, int lsum, vector<vector<long long int>>&dp){
+    //     if(lsum == 0){
+    //         return 1;
+    //     }
+        
+    //     if(index == 0){
+    //         return 0;
+    //     }
+        
+    //     if(dp[lsum][index] == -1){
+    //         long long int p = 0, np = 0;
+            
+    //         if(coins[index - 1] <= lsum){
+    //             p = func(coins, index, lsum - coins[index - 1], dp);
+    //         }
+    //         np = func(coins, index - 1, lsum, dp);
+            
+    //         dp[lsum][index] = p + np;
+    //     }
+    //     return dp[lsum][index];
+    // }
 };
 
 //{ Driver Code Starts.
