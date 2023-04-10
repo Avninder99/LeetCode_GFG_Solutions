2ce86fd992a579ca43523e8a513424335f6069e0
Avninder Preet Singh <avninderpreet99@gmail.com>
2023-04-11 03:58:42 +0530

    Added solution - LeetHub

--- Partition Equal Subset Sum - GFG/partition-equal-subset-sum.cpp
+++ Partition Equal Subset Sum - GFG/partition-equal-subset-sum.cpp
@@ -20,29 +20,58 @@ public:
             return false;
         }
         sum /= 2;
-        vector<vector<int>>dp(sum + 1, vector<int>(N + 1, -1));
-        return func(N, sum, arr, dp);
-    }
-    bool func(int index, int lsum, int arr[], vector<vector<int>>&dp){
-        if(lsum == 0){
-            return true;
-        }else if(index == 0){
-            return false;
+        
+        // DP Memoization
+        // vector<vector<int>>dp(sum + 1, vector<int>(N + 1, -1));
+        // return func(N, sum, arr, dp);
+        
+        
+        // DP Tabulation
+        vector<vector<int>>t(sum + 1, vector<int>(N + 1, -1));
+        
+        for(int i=0;i<=sum;i++){
+            t[i][0] = 0;
+        }
+        for(int i=0;i<=N;i++){
+            t[0][i] = 1;
         }
         
-        if(dp[lsum][index] == -1){
-            bool p = false, np = false;
-            if(arr[index - 1] <= lsum){
-                p = func(index - 1, lsum - arr[index - 1], arr, dp);
+        for(int i=1;i<=sum;i++){
+            for(int j=1;j<=N;j++){
+                bool p = false, np = false;
+                if(arr[j - 1] <= i){
+                    p = t[i - arr[j - 1]][j - 1];
+                }
+                if(!p){
+                    np = t[i][j - 1];
+                }
+                t[i][j] = (p || np) ? 1 : 0;
             }
-            if(!p){
-                np = func(index - 1, lsum, arr, dp);
-            }
-            dp[lsum][index] = (p || np) ? 1 : 0;
         }
         
-        return dp[lsum][index];
+        return t[sum][N];
     }
+    
+    // bool func(int index, int lsum, int arr[], vector<vector<int>>&dp){
+    //     if(lsum == 0){
+    //         return true;
+    //     }else if(index == 0){
+    //         return false;
+    //     }
+        
+    //     if(dp[lsum][index] == -1){
+    //         bool p = false, np = false;
+    //         if(arr[index - 1] <= lsum){
+    //             p = func(index - 1, lsum - arr[index - 1], arr, dp);
+    //         }
+    //         if(!p){
+    //             np = func(index - 1, lsum, arr, dp);
+    //         }
+    //         dp[lsum][index] = (p || np) ? 1 : 0;
+    //     }
+        
+    //     return dp[lsum][index];
+    // }
 };
 
 //{ Driver Code Starts.
