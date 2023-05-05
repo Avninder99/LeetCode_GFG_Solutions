1e075ebcbc5bf4b2aadf559851b91d0b6aecda55
Avninder Preet Singh <avninderpreet99@gmail.com>
2023-05-05 15:49:16 +0530

    Time: 79 ms (19.16%), Space: 43.3 MB (62.96%) - LeetHub

---
+++ 2629-function-composition/2629-function-composition.js
@@ -0,0 +1,17 @@
+/**
+ * @param {Function[]} functions
+ * @return {Function}
+ */
+var compose = function(functions) {
+	return function(x) {
+        functions.reverse().forEach((func) => {
+            x = func(x);
+        })
+        return x;
+    }
+};
+
+/**
+ * const fn = compose([x => x + 1, x => 2 * x])
+ * fn(4) // 9
+ */
