555b6cf9110ce0d621de9ddb736521ababe73fdd
Avninder Preet Singh <avninderpreet99@gmail.com>
2023-05-16 19:25:43 +0530

    Time: 138 ms (5.07%), Space: 24.6 MB (12.81%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,11 +1,27 @@
 class Solution {
 public:
-    // NOT MINE (DO THIS)
-    int longestCommonSubsequence(string &a, string &b) {
-        short m[1001][1001] = {};
-        for (auto i = 0; i < a.size(); ++i)
-            for (auto j = 0; j < b.size(); ++j)
-                m[i + 1][j + 1] = a[i] == b[j] ? m[i][j] + 1 : max(m[i + 1][j], m[i][j + 1]);
-        return m[a.size()][b.size()];
+    int longestCommonSubsequence(string t1, string t2) {
+        int l1 = t1.length(), l2 = t2.length();
+        vector<vector<int>>dp(l1 + 1, vector<int>(l2 + 1, -1));
+        return func(l1, l2, t1, t2, dp);
+    }
+    int func(int ind1, int ind2, string &t1, string &t2, vector<vector<int>>&dp){
+        // base case
+        if(ind1 == 0 || ind2 == 0){
+            return 0;
+        }
+        
+        if(dp[ind1][ind2] == -1){
+            // choice diagram code
+            if(t1[ind1 - 1] == t2[ind2 - 1]){
+                dp[ind1][ind2] = 1 + func(ind1 - 1, ind2 - 1, t1, t2, dp);
+            }else{
+                int f, s;
+                f = func(ind1 - 1, ind2, t1, t2, dp);
+                s = func(ind1, ind2 - 1, t1, t2, dp);
+                dp[ind1][ind2] = max(f, s);
+            }
+        }
+        return dp[ind1][ind2];
     }
 };
