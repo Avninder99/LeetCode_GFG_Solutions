a3eb1dfdabcc523b8ce7903511bf8cb16f2235ac
Avninder Preet Singh <avninderpreet99@gmail.com>
2023-05-21 18:51:11 +0530

    Time: 121 ms (10.99%), Space: 28.4 MB (42.43%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -1,25 +1,19 @@
 class Solution {
 public:
     int minInsertions(string s) {
-        int l = 0, r = s.length() - 1;
-        vector<vector<int>>dp(s.length() + 1, vector<int>(s.length() + 1, -1));
-        return func(s, l, r, dp);
-    }
-    int func(string &s, int l, int r, vector<vector<int>>&dp){
-        if(l == r || (l == r - 1 && s[l] == s[r])){
-            return 0;
-        }
-        if(dp[l][r] == -1){
-            int lp = 0, rp = 0, np = 0;
-            if(s[l] == s[r]){
-                np = func(s, l + 1, r - 1, dp);
-                dp[l][r] = np;
-            }else{
-                lp = 1 + func(s, l + 1, r, dp);
-                rp = 1 + func(s, l, r - 1, dp);
-                dp[l][r] = min(lp, rp);
+        string rev = s;
+        reverse(rev.begin(), rev.end());
+        int len = s.length();
+        vector<vector<int>>dp(len + 1, vector<int>(len + 1, 0));
+        for(int i=1;i<=len;i++){
+            for(int j=1;j<=len;j++){
+                if(s[i - 1] == rev[j - 1]){
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                }else{
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                }
             }
         }
-        return dp[l][r];
+        return len - dp[len][len];
     }
 };
