ed7bd3e3bee482b0409cfedd18a3bef75767629c
Avninder Preet Singh <avninderpreet99@gmail.com>
2023-05-22 00:06:07 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (96.50%) - LeetHub

---
+++ 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int kthFactor(int n, int k) {
+        int place = 0;
+        for(int i=1;i<=n;i++){
+            if(n%i == 0){
+                k--;
+            }
+            if(k == 0){
+                return i;
+            }
+        }
+        return -1;
+    }
+};
