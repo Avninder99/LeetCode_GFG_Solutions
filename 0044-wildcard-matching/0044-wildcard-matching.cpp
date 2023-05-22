0e1b726cd2097fc72f13c9bdf0da88ee182325ef
Avninder Preet Singh <avninderpreet99@gmail.com>
2023-05-23 00:29:53 +0530

    Time: 91 ms (41.23%), Space: 9.5 MB (81.78%) - LeetHub

---
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        int pLen = p.length(), sLen = s.length();
+        vector<vector<bool>>dp(pLen + 1, vector<bool>(sLen + 1, false));
+        dp[0][0] = true;
+        for(int i=1;i<=pLen;i++){
+            for(int j=0;j<=sLen;j++){
+                if(j == 0){
+                    if(p[i - 1] == '*'){
+                        dp[i][j] = dp[i - 1][j];
+                    }else{
+                        dp[i][j] = false;
+                    }
+                }
+                else if(p[i - 1] == s[j - 1] || p[i - 1] == '?'){
+                    dp[i][j] = dp[i - 1][j - 1];
+                }
+                else if(p[i - 1] == '*'){
+                    dp[i][j] = dp[i - 1][j] || dp[i - 1][j - 1] || dp[i][j - 1];
+                }
+                else{
+                    dp[i][j] = false;
+                }
+            }
+        }
+        return dp[pLen][sLen];
+    }
+};
