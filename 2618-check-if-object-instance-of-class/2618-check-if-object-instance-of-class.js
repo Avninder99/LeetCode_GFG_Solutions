a4d7303b098e5b0fc922e6b5eb95ab7000c805a6
Avninder Preet Singh <avninderpreet99@gmail.com>
2023-06-04 03:30:16 +0530

    Time: 103 ms (75.71%), Space: 52.5 MB (29.41%) - LeetHub

--- 2618-check-if-object-instance-of-class/2618-check-if-object-instance-of-class.js
+++ 2618-check-if-object-instance-of-class/2618-check-if-object-instance-of-class.js
@@ -4,13 +4,17 @@
  * @return {boolean}
  */
 var checkIfInstanceOf = function(obj, classFunction) {
-    while(obj != null && obj != undefined){
-        if(obj.constructor === classFunction){
-             return true;
+    while(obj!=null)
+    {
+        if(obj.constructor === classFunction)
+        {
+            return true;
         }
+
         obj = Object.getPrototypeOf(obj);
-        
+
     }
+
     return false;
 };
 
